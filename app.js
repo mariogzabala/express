f618913b43684ce0182fcd02dd9337653fd1d5df
Mario Zabala <mariogzabala@gmail.com>
2022-01-03 19:46:44 -0300

    definimos rutas para las respuestas de la aplicacion

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
 const app = express();
 
 app.listen(8000,() =>
     console.log("Levantando un servidor con Express"));
+
+app.get('/', (req,res) => {
+    res.send('¡Hola mundo!');
+});    
