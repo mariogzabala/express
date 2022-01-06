71afc2481fe1415e519cbc0f1ad0f61abb44bd6c
Mario Zabala <mariogzabala@gmail.com>
2022-01-06 12:00:49 -0300

    agrego funciones

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 const express = require('express');
+const path = require('path')
 const req = require('express/lib/request');
 const res = require('express/lib/response');
 const app = express();
 
-app.listen(8000,() =>
-    console.log("Levantando un servidor con Express"));
+app.listen(8000,() => console.log("Levantando un servidor con Express"));
 
 app.get('/', (req,res) => {
     res.send('¡Hola mundo!');
-});    
+})
+
+app.get('/contacto', function(req, res){
+   res.send('Registrate') 
+})
+
+let saludo = "Bienvenido/a, ahora estamos en tu perfil"
+app.get('/perfil', function(req,res){
+    res.send(saludo)
+});
+
+let producto = {
+    tipoProducto: null, 
+    precio: null, 
+    cantidad: null
+};
+app.get('/producto/agregar', (req,res) => {
+    res.send(producto)
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/about-us.html'))
+});
